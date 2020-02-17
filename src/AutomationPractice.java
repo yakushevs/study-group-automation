9ddfd9dc50616b3222c7f051879a94f26737b278
yakushevs <juandjar@gmail.com>
2020-02-17 14:33:12 -0500

    con el wait de java

--- src/AutomationPractice.java
+++ src/AutomationPractice.java
@@ -3,50 +3,41 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+import static org.openqa.selenium.WebDriver.*;
 
 public class AutomationPractice {
     public static void main(String[] args) throws Exception{
 
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver,2);
+        System.out.println("Ir a la pagina");
         driver.navigate().to("http://automationpractice.com/index.php?");
         driver.manage().window().maximize();
-        WebElement product= driver.findElement(By.xpath("//ul[@id='homefeatured']//div[@class='product-container'][1]"));
+
+        JavascriptExecutor js= (JavascriptExecutor) driver;
+        System.out.println("Scroll down");
+        js.executeScript("window.scrollTo(0,1200)");
+
+        WebElement product= driver.findElement(By.xpath("//ul[@id='homefeatured']//li[contains(@class,'ajax_block_product')][7]"));
         Actions over=new Actions(driver);
         over.moveToElement(product).perform();
-        WebElement locator= driver.findElement(By.xpath("//a[@title='Add to cart']"));
-        locator.click();
+
+        WebElement addbutton= product.findElement(By.xpath("div//a[contains(@class,'ajax_add_to_cart_button')]"));
+
+    System.out.println("Click");
+        addbutton.click();
+        Thread.sleep(5000);
+        System.out.println("Esperar");
 
 
 
-
-
-//        WebElement we= WebDriver.findElement(By.xpath("//a[@title='Add to cart']")).click().build().perform();
-        //product.click();
-
-
-       /* Actions builder = new Actions(driver);
-builder.moveToElement(hoverElement).perform();
-By locator = By.id("clickElementID");
-driver.click(locator);
-
-       Actions builder = new Actions(driver);
-builder.moveToElement(hoverElement).perform();
-builder.moveToElement(clickElement).click().perform();
-
-
-       Actions action = new Actions(webdriver);
-        WebElement we = webdriver.findElement(By.xpath("html/body/div[13]/ul/li[4]/a"));
-        action.moveToElement(we).moveToElement(webdriver.findElement(By.xpath("/expression-here"))).click().build().perform();
-
-
-        product.over
-    //a[@title='Add to cart']
-        product.click();
-        WebElement addToCartButton= driver.findElement(By.id("add-to-cart-button"));
-        addToCartButton.click();
-        WebDriver.Timeouts timeouts = driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MICROSECONDS);
-        driver.close();*/
+            driver.close();
 
 
 
