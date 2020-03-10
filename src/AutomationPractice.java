19358d1c15df2775e9e25df3e66f2143b7516ee9
yakushevs <juandjar@gmail.com>
2020-03-10 15:55:39 -0500

    empezando constructor

--- src/AutomationPractice.java
+++ src/AutomationPractice.java
@@ -7,10 +7,12 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Currency;
 import java.util.concurrent.TimeUnit;
 import static org.openqa.selenium.WebDriver.*;
 
 public class AutomationPractice {
+
     public static WebDriver configuracionDriver(String pagina) {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
@@ -20,54 +22,57 @@ public class AutomationPractice {
         driver.manage().window().maximize();
         return driver;
     };
+
+    class Producto {
+
+        //propiedades
+        private boolean mouseOverStatus;
+        private WebElement btnAddToCart, btnQuickView, btnMore, imgProducto;
+        private float valor;
+        private String nombreProducto, categoriaProducto;
+
+        Producto () {
+        }
+
+        //Acciones o metodos
+        public static seleccionar(int indexProducto) {
+        }
+
+        public static adicionar(int indexProducto){
+        }
+   }
+
     public static void main(String[] args) throws Exception{
 
         WebDriver driver= configuracionDriver("http://automationpractice.com/index.php?");
-        /*
-        JavascriptExecutor js= (JavascriptExecutor) driver;
-        System.out.println("Adicionar producto popular");
-        js.executeScript("window.scrollTo(0,1200)");
 
-        WebElement product= driver.findElement(By.xpath("//ul[@id='homefeatured']//li[contains(@class,'ajax_block_product')][7]"));
         Actions over=new Actions(driver);
+
+        WebElement tabPopular=driver.findElement(By.xpath("//*[@class=\"homefeatured\"]"));
+        tabPopular.click();
+        WebElement product= driver.findElement(By.xpath("//ul[@id='homefeatured']//li[contains(@class,'ajax_block_product')][7]"));
         over.moveToElement(product).perform();
-
-        WebElement addbutton= product.findElement(By.xpath("div//a[contains(@class,'ajax_add_to_cart_button')]"));
-
-
-    System.out.println("Producto popular adicionado");
-        addbutton.click();
+        WebElement addToCartButton= product.findElement(By.xpath("div//a[contains(@class,'ajax_add_to_cart_button')]"));
+        addToCartButton.click();
         Thread.sleep(5000);
         WebElement btnContinue = driver.findElement(By.xpath("//span[contains(@class,'continue')]"));
         btnContinue.click();
-        System.out.println("continue shopping");
-
-//--------
-        System.out.println("Seleccionar Best Sellers");
         Thread.sleep(5000);
-        WebElement tabBestseller=driver.findElement(By.xpath("//*[@id=\"home-page-tabs\"]/li[2]/a"));
 
+
+        WebElement tabBestseller=driver.findElement(By.xpath("//*[@class=\"blockbestsellers\"]"));
         tabBestseller.click();
-        System.out.println("Seleccionar producto");
         WebElement productBeste= driver.findElement(By.xpath("//ul[@id='blockbestsellers']//li[contains(@class,'ajax_block_product')][4]"));
         over.moveToElement(productBeste).perform();
-        Thread.sleep(5000);
-
         WebElement btnAddBestie = productBeste.findElement(By.xpath("div//a[contains(@class,'ajax_add_to_cart_button')]"));
         btnAddBestie.click();
-        Thread.sleep(5000);
-        System.out.println("Producto Best Seller adicionado");
-
-
         Thread.sleep(5000);
         WebElement btnContinueBestie = productBeste.findElement(By.xpath("//span[contains(@class,'continue')]"));
         btnContinueBestie.click();
         Thread.sleep(5000);
-        System.out.println("continue shopping on best selling");
 
-////ul[@id='blockbestsellers']//li[contains(@class,'ajax_block_product')][5]/div//a[contains(@class,'ajax_add_to_cart')]
 
-//--- Segundo producto de best seller
+        //--- Segundo producto de best seller
         System.out.println("Seleccionar producto");
         productBeste= driver.findElement(By.xpath("//ul[@id='blockbestsellers']//li[contains(@class,'ajax_block_product')][6]"));
         over.moveToElement(productBeste).perform();
